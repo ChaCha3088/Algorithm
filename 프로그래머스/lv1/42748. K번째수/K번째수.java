5f054192dc8d474ea32c845543a9fc98031fea8c
ChaCha3088 <90785316+ChaCha3088@users.noreply.github.com>
2023-02-13 15:26:56 +0900

    [level 1] Title: K번째수, Time: 0.25 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            
+            List<Integer> arrayList = new ArrayList<>();
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                arrayList.add(array[j]);
+            }
+            
+            Collections.sort(arrayList);
+            answer[i] = arrayList.get(commands[i][2] - 1);
+
+        }
+        
+        
+        return answer;
+    }
+}
