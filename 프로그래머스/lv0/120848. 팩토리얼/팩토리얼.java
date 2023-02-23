d4b273f2fe04dfa6f2d2cef17574dfcdc5d468cd
ChaCha3088 <90785316+ChaCha3088@users.noreply.github.com>
2023-02-23 14:10:03 +0900

    Update 팩토리얼.java

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -18,14 +18,4 @@ class Solution {
         
         return answer;
     }
-    
-    private int factorial(int input) {
-        int result = 1;
-        
-        for (int i = 1; i <= input; i++) {
-            result *= i;
-        }
-        
-        return result;
-    }
 }
