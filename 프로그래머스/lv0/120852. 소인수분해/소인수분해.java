4d34bb6dddc1d67ea393f2eb521c643d63e8f036
ChaCha3088 <90785316+ChaCha3088@users.noreply.github.com>
2023-02-24 19:20:58 +0900

    [level 0] Title: 소인수분해, Time: 1.98 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int target = n;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 2; i <= target; i++) {
+            if (target == 1) {
+                break;
+            }
+            while (target % i == 0) {
+                System.out.println(target);
+                target /= i;
+                set.add(i);
+                System.out.println(i);
+                System.out.println(target);
+            }
+        }
+        
+        List<Integer> list = new ArrayList<>(set);
+        list.stream()
+            .sorted()
+            .collect(Collectors.toList());
+        
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
