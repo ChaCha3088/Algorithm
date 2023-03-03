2cd9fc0bc7b6e7961444847086f7d2903f7a9ff2
ChaCha3088 <90785316+ChaCha3088@users.noreply.github.com>
2023-03-03 17:28:35 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -2,8 +2,6 @@
 -- 진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시 항목이 출력
 -- 결과는 진료예약일시를 기준으로 오름차순 정렬
 
-
-
 SELECT AP.APNT_NO, PT.PT_NAME, PT.PT_NO, DR.MCDP_CD, DR.DR_NAME, AP.APNT_YMD
   FROM PATIENT PT, DOCTOR DR, APPOINTMENT AP
   WHERE PT.PT_NO = AP.PT_NO AND DR.MCDP_CD = AP.MCDP_CD AND DR.DR_ID = AP.MDDR_ID
