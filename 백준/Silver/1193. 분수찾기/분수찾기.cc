e44de34a39b957539544454e45af9daeed3f6ca9
Cha Cha <cha3088@gmail.com>
2023-04-12 10:31:21 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+#define fastIo cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+
+using namespace std;
+
+int main() {
+    fastIo;
+
+    //입력
+    int X;
+    cin >> X;
+
+    int start = sqrt((2 * X));
+
+    while (start * (start + 1) / 2 < X) {
+        start++;
+    }
+
+    int diff = start * (start + 1) / 2 - X;
+
+    if (start % 2 == 0) {
+        cout << start - diff << "/" << 1 + diff;
+    } else {
+        cout << 1 + diff << "/" << start - diff;
+    }
+
+    return 0;
+}
