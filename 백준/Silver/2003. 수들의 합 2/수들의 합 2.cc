a48fe93e8cc674f3161cdfba25f6cdc7e6bd95a7
Cha Cha <cha3088@gmail.com>
2023-04-13 13:44:28 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using ll = long long;
+#define fastIo cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+
+using namespace std;
+
+int arr[100001];
+
+int main() {
+    fastIo;
+
+    //입력
+    int N;
+    ll M;
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
+    }
+
+    ll answer = 0;
+
+    int i = 0;
+    int j = 0;
+    ll sum = 0;
+
+    while (j <= N) {
+        if (sum < M) {
+            sum += arr[j++];
+        } else if (sum >= M) {
+            sum -= arr[i++];
+        }
+
+        if (sum == M) {
+            answer++;
+        }
+    }
+
+    cout << answer;
+}
