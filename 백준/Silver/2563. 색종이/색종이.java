f7780d46d1094d65f6b326dbb851a9741ae90ce4
Cha Cha <cha3088@gmail.com>
2023-07-05 13:43:12 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,53 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int sum = 0;
+		
+		int[][]map = new int[101][101];
 
-    private static int[][] map;
-    private static int[][] arr;
-    private static int N;
-    private static StringBuilder sb;
-    private static boolean result;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < 10; k++) {
+					map[x + j][y + k] = 1;
+				}
+			}
+		}
+		
+		for (int i = 1; i <= 100; i++) {
+			for (int j = 1; j <= 100; j++) {
+				if (map[i][j] == 1) {
+					sum += 1;
+				}
+			}
+		}
+		
+		sb.append(sum);
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-
-        map = new int[101][101];
-        arr = new int[N + 1][2];
-
-        //입력
-        for (int i = 1; i <= N; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
-        }
-
-        for (int i = 1; i <= N; i++) {
-            int x = arr[i][0];
-            int y = arr[i][1];
-
-            for (int j = x; j < x + 10; j++) {
-                for (int k = y; k < y + 10; k++) {
-                    map[j][k] = 1;
-                }
-            }
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                 if (map[i][j] == 1) {
-                     sum += 1;
-                 }
-            }
-        }
-
-        //출력
-        System.out.println(sum);
-    }
+		//출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+	}
 }
