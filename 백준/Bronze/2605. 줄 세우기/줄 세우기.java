d7ec95a7651c92072c72a462ca23ed02a05117b4
Cha Cha <cha3088@gmail.com>
2023-07-05 12:15:00 +0900

    [Bronze II] Title: 줄 세우기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.java
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -1,50 +1,54 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		List<Integer> input = new ArrayList();
+		input.add(0);
+				
+		List<Integer> lst = new ArrayList();
+		lst.add(0);
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			lst.add(i);
+			input.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		
+		for (int i = 2; i <= N; i++) {
+            int number = input.get(i);
 
-    private static List<Integer> arr;
-    private static List<Integer> numbers;
-    private static int N;
-    private static StringBuilder sb;
-    private static boolean result;
+            int idx = lst.indexOf(i);
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
+            lst.remove(i);
 
-        N = sc.nextInt();
-
-        arr = new ArrayList();
-        numbers = new ArrayList();
-
-        arr.add(0);
-        numbers.add(0);
-
-        //입력
-        for (int i = 1; i <= N; i++) {
-            arr.add(i);
-            numbers.add(sc.nextInt());
+            lst.add(idx - number, i);
         }
-
-        for (int i = 2; i <= N; i++) {
-            int number = numbers.get(i);
-
-            int idx = arr.indexOf(i);
-
-            arr.remove(i);
-
-            arr.add(idx - number, i);
-        }
-
-        //출력
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            sb.append(arr.get(i)).append(" ");
-        }
-
-        System.out.println(sb);
-    }
+		
+		for (int i = 1; i <= N; i++) {
+			sb.append(lst.get(i)).append(" ");
+		}
+		
+		//출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+	}
 }
