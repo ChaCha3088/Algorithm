3d729f863974a0bf4f87ae58fa41000d5f987b93
Cha Cha <cha3088@gmail.com>
2023-07-29 22:40:29 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 152 ms, Memory: 15720 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.valueOf(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String string = "";
+
+            for (int j = 0; j < i; j++) {
+                string += " ";
+            }
+
+            for (int j = 0; j < N - i; j++) {
+                string += "*";
+            }
+
+            System.out.println(string);
+        }
+    }
+}
