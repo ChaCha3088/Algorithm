aac7a146d4902511a3a0c1dd9d52376d5be7afe9
Cha Cha <cha3088@gmail.com>
2023-08-01 17:19:05 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,83 +1,102 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    private static int N;
-    private static int[] switches = new int[101];
+    private static int[] arr;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-
-        //입력
-        for (int i = 1; i <= N; i++) {
-            switches[i] = sc.nextInt();
-        }
-
-        int studentNumber = sc.nextInt();
-
-        for (int i = 0; i < studentNumber; i++) {
-             int sex = sc.nextInt();
-             int number = sc.nextInt();
-
-             if (sex == 1) {
-                 man(number);
-             } else if (sex == 2) {
-                 woman(number);
-             }
-        }
-
-        //출력
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i <= N; i++) {
-            sb.append(switches[i]);
+        // 스위치 개수
+        int N = Integer.valueOf(br.readLine());
 
+        // 배열 초기화
+        arr = new int[N + 1];
+
+        // 스위치 상태 입력
+        String[] split = br.readLine().split(" ");
+        for (int i = 0; i < split.length; i++) {
+            arr[i + 1] = Integer.valueOf(split[i]);
+        }
+
+        // 학생 수
+        int students = Integer.valueOf(br.readLine());
+
+        for (int i = 0; i < students; i++) {
+            String[] split1 = br.readLine().split(" ");
+
+            int sex = Integer.valueOf(split1[0]);
+            int number = Integer.valueOf(split1[1]);
+
+            // 남학생은
+            if (sex == 1) {
+                for (int j = number; j <= N; j += number) {
+                    // 켜져 있으면
+                    if (arr[j] == 1) {
+                        // 끈다.
+                        arr[j] = 0;
+                    }
+                    // 꺼져 있으면
+                    else {
+                        // 켠다.
+                        arr[j] = 1;
+                    }
+                }
+            }
+            // 여학생은
+            else {
+                int howFar = 1;
+
+                while (true) {
+                    // 스위치 범위 내에 있을 때만
+                    if (number - howFar >= 1 && number + howFar <= N) {
+                        // 범위 넓혀간 두 스위치 상태가 같으면
+                        if (arr[number - howFar] == arr[number + howFar]) {
+                            // 범위를 넓힌다.
+                            howFar += 1;
+                        }
+                        // 다르면 반복문을 종료한다.
+                        else {
+                            howFar -= 1;
+                            break;
+                        }
+                    }
+                    // 스위치 범위 밖이면
+                    else {
+                        howFar -= 1;
+                        break;
+                    }
+                }
+
+                // 얼마나 확장할 수 있는지 결정되면
+                // number - howFar부터 number + howFar까지 상태를 바꾼다.
+                for (int j = number - howFar; j <= number + howFar; j++) {
+                    // 켜져 있었으면
+                    if (arr[j] == 1) {
+                        // 끄고
+                        arr[j] = 0;
+                    }
+                    // 꺼져 있었으면
+                    else {
+                        // 켠다.
+                        arr[j] = 1;
+                    }
+                }
+            }
+        }
+
+        // 마지막 상태를
+        // 출력
+        for (int i = 1; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
             if (i % 20 == 0) {
                 sb.append("\n");
-            } else {
-                sb.append(" ");
             }
         }
 
         System.out.println(sb);
-    }
 
-    private static void man(int number) {
-        int currentNumber = number;
-
-        while (currentNumber <= N) {
-            if (switches[currentNumber] == 1) {
-                switches[currentNumber] = 0;
-            } else {
-                switches[currentNumber] = 1;
-            }
-
-            currentNumber += number;
-        }
-    }
-
-    private static void woman(int number) {
-        int up = number;
-        int down = number;
-
-        while (up + 1 <= N && down - 1 > 0) {
-            if (switches[up + 1] != switches[down - 1]) {
-                break;
-            }
-
-            up += 1;
-            down -= 1;
-        }
-
-        for (int i = down; i <= up; i++) {
-            if (switches[i] == 1) {
-                switches[i] = 0;
-            } else {
-                switches[i] = 1;
-            }
-        }
+        // StringBuilder 초기화
+//        sb = new StringBuilder();
     }
 }
