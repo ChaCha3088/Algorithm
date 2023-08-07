273a5936d4ebfd122e4a7b64a453a1175b1c8d37
Cha Cha <cha3088@gmail.com>
2023-08-07 13:42:48 +0900

    [Gold V] Title: 탑, Time: 664 ms, Memory: 92592 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,75 +1,54 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb = new StringBuilder();
-    private static int[] arr;
-    private static int[] results;
 
-    static class IndexAndValue {
-        int index;
-        int value;
-
-        public IndexAndValue(int index, int value) {
-            this.index = index;
-            this.value = value;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+        // TODO Auto-generated method stub
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 탑에서 발사된 레이저 신호는 가장 먼저 만나는 단 하나의 탑에서만 수신이 가능
-
-        // N 입력
         int N = Integer.parseInt(br.readLine());
+        int[] towers = new int[N + 1];
+        int[] result = new int[N + 1];
+        // Double Ended Queue
+        // LinkedList
+        // 이유
+        // 
+        Deque<int[]> stack = new LinkedList<>();
 
-        // 배열 초기화
-        arr = new int[N + 1];
-        results = new int[N + 1];
-
-        // 배열 입력
-        String[] split = br.readLine().split(" ");
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(split[i - 1]);
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            towers[i] = Integer.parseInt(st.nextToken());
         }
 
-        // stack 선언
-        Stack<IndexAndValue> stack = new Stack<>();
+        int tower;
+        for(int i = 1; i <= N; i++) {
+            tower = towers[i];
 
-        // 첫번째 값을 넣어준다.
-        stack.push(new IndexAndValue(1, arr[1]));
-
-        // result[1]은 무조건 0
-        results[1] = 0;
-
-        // index 2부터 시작
-        for (int i = 2; i <= N; i++) {
-            // top보다 큰 값이 들어오면
-            if (stack.peek().value < arr[i]) {
-                // arr[i] 작은 값들 다 빼줌
-                while (stack.size() > 0 && stack.peek().value < arr[i]) {
-                    stack.pop();
+            while(!stack.isEmpty()) {
+                if(stack.peek()[1] > tower) {
+                    break;
                 }
+                stack.pollFirst();
             }
 
-            // top보다 작은 값이 들어오면
-            // 결과 기록
-            if (stack.size() > 0) {
-                IndexAndValue peek = stack.peek();
-                results[i] = peek.index;
+            if(stack.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek()[0];
             }
 
-            // 값을 넣어줌
-            stack.push(new IndexAndValue(i, arr[i]));
+            stack.addFirst(new int[] {i, tower});
         }
-
-        for (int i = 1; i <= N; i++) {
-            sb.append(results[i]).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            sb.append(result[i]).append(" ");
         }
+        sb.append("\n");
 
         System.out.println(sb);
     }
