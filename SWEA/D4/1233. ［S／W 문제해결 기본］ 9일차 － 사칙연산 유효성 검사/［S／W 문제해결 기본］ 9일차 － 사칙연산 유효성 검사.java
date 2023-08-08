072c463763b1b2a56650457539029023aa46111f
Cha Cha <cha3088@gmail.com>
2023-08-08 14:17:02 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 132 ms, Memory: 20,268 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -1,39 +1,60 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Solution {
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+public class Solution {
+    private static StringBuilder sb = new StringBuilder();
 
-		int T = 10;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int N = Integer.parseInt(br.readLine());
-			
-			int answer = 1;
+        for (int test = 1; test <= 10; test++) {
+            int N = Integer.parseInt(br.readLine());
 
-			for(int i = 1; i <= N; i++) {
-				StringTokenizer st =  new StringTokenizer(br.readLine());
-				
-				st.nextToken();
-				
-				char node = st.nextToken().charAt(0); 
-				
-				if(st.hasMoreTokens()) {
-					if(node >= '0' && node <= '9') {
-						answer = 0;
-					}
-				} else {
-					if(node < '0' || node > '9') {
-						answer = 0;
-					}
-				}
-			}
-			sb.append("#" + test_case + " " + answer + "\n");
-		}
-		System.out.println(sb.toString());
-	}
+            int answer = 0;
+
+            int where = 0;
+
+            for (int n = 1; n <= N; n++) {
+                String[] split = br.readLine().split(" ");
+
+                // 리프 노드
+                if (split.length == 2) {
+                    try {
+                        Integer.parseInt(split[1]);
+                    } catch (NumberFormatException e) {
+                        where = n;
+                        break;
+                    }
+                }
+                // 리프 노드가 아닐 때
+                else {
+                    try {
+                        Integer number = Integer.parseInt(split[1]);
+                        if (number instanceof Integer) {
+                            where = n;
+                            break;
+                        }
+                    } catch (NumberFormatException e) {
+                    }
+                }
+
+                if (n == N) {
+                    answer = 1;
+                }
+            }
+
+            if (answer == 0 && where < N) {
+                for (int j = where + 1; j <= N; j++) {
+                    br.readLine();
+                }
+            }
+
+            sb.append("#").append(test).append(" ").append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
