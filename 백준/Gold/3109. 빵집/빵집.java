39fa8537521474fa0d148c17fef6366225841077
Cha Cha <cha3088@gmail.com>
2023-08-17 11:38:34 +0900

    [Gold II] Title: 빵집, Time: 408 ms, Memory: 44384 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Main {
-    private static int[][] arr;
+    private static char[][] map;
     private static int answer = 0;
     private static int R;
     private static int C;
@@ -15,20 +15,14 @@ public class Main {
         R = Integer.parseInt(split[0]);
         C = Integer.parseInt(split[1]);
 
-        arr = new int[R + 1][C + 1];
+        map = new char[R + 1][C + 1];
 
         for (int r = 1; r <= R; r++) {
-            split = br.readLine().split("");
-
-            for (int c = 1; c <= C; c++) {
-                if (split[c - 1].equals("x")) {
-                    arr[r][c] = 1;
-                }
-            }
+            map[r] = br.readLine().toCharArray();
         }
 
         for (int y = 1; y <= R; y++) {
-            if (dfs(1, y)) {
+            if (dfs(0, y)) {
                 answer += 1;
             }
         }
@@ -37,16 +31,16 @@ public class Main {
     }
 
     private static boolean dfs(int x, int y) {
-        arr[y][x] = 1;
+        map[y][x] = 'x';
 
-        if (x == C) {
+        if (x == C - 1) {
             return true;
         }
 
         // 가능한거에서
         for (int d = 0; d < 3; d++) {
             // 지도 범위 내 & 건물로 막혀있지 않을 것
-            if (x + 1 <= C && y + dy[d] <= R && y + dy[d] >= 1 && arr[y + dy[d]][x + 1] == 0) {
+            if (x + 1 <= C - 1 && y + dy[d] <= R && y + dy[d] >= 1 && map[y + dy[d]][x + 1] == '.') {
                 // 탐색하고
                 if (dfs(x + 1, y + dy[d])) {
                     return true;
