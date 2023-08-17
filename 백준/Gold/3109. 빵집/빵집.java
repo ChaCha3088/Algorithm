c98aa2334bfa5f68f3b9b48db9fccfb0cd93f69f
Cha Cha <cha3088@gmail.com>
2023-08-17 13:50:56 +0900

    [Gold II] Title: 빵집, Time: 408 ms, Memory: 34196 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -15,25 +15,22 @@ public class Main {
         R = Integer.parseInt(split[0]);
         C = Integer.parseInt(split[1]);
 
-        map = new char[R + 1][C + 1];
+        map = new char[R + 1][];
 
         for (int r = 1; r <= R; r++) {
             map[r] = br.readLine().toCharArray();
         }
 
         for (int y = 1; y <= R; y++) {
-            if (dfs(0, y)) {
-                answer += 1;
-            }
+            dfs(0, y);
         }
 
         System.out.println(answer);
     }
 
     private static boolean dfs(int x, int y) {
-        map[y][x] = 'x';
-
         if (x == C - 1) {
+            answer += 1;
             return true;
         }
 
@@ -41,6 +38,8 @@ public class Main {
         for (int d = 0; d < 3; d++) {
             // 지도 범위 내 & 건물로 막혀있지 않을 것
             if (x + 1 <= C - 1 && y + dy[d] <= R && y + dy[d] >= 1 && map[y + dy[d]][x + 1] == '.') {
+                map[y][x] = 'x';
+
                 // 탐색하고
                 if (dfs(x + 1, y + dy[d])) {
                     return true;
