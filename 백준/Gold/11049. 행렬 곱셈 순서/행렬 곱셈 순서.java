4057e57130633fd315a82d44c5b1ecb7e779a6db
Cha Cha <cha3088@gmail.com>
2023-09-14 00:48:44 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 280 ms, Memory: 18684 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[][] arr;
+    private static int[][] dp;
+    private static int N;
+    private static long answer = Long.MAX_VALUE;
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        // 배열 초기화
+        arr = new int[N][2];
+        dp = new int[N][N];
+
+        // 입력
+        for (int n = 1; n <= N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            arr[n - 1][0] = Integer.parseInt(st.nextToken());
+            arr[n - 1][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            dp[i][i + 1] = arr[i][0] * arr[i][1] * arr[i + 1][1];
+        }
+
+        for (int diff = 2; diff < N; diff++) {
+            for (int i = 0; i + diff < N; i++) {
+                int j = i + diff;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + arr[i][0] * arr[k][1] * arr[j][1]);
+                }
+            }
+        }
+
+        System.out.println(dp[0][N - 1]);
+    }
+}
