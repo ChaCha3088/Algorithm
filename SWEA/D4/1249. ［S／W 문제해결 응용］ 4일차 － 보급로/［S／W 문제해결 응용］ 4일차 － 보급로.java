1751f3bf7930e6a09922b844b6c7cc7cb9ff9554
Cha Cha <cha3088@gmail.com>
2023-10-11 19:21:55 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 212 ms, Memory: 29,288 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,98 +1,81 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
- 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Solution {
-    private static int T, N;
-    private static int[][] min;
-    private static int[][] map;
-    private static LinkedList<Integer> queue;
-    private static final int[] dx = {-1, 1, 0, 0};
-    private static final int[] dy = {0, 0, -1, 1};
- 
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
- 
-        //입력
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
- 
- 
-        for (int i = 0; i < T; i++) {
-            queue = new LinkedList<>();
- 
-            //입력
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
- 
-            map = new int[N + 1][N + 1];
- 
-            for (int j = 1; j <= N; j++) {
-                st = new StringTokenizer(br.readLine());
-                String[] split = st.nextToken().split("");
-                for (int k = 1; k <= split.length; k++) {
-                    map[j][k] = Integer.parseInt(split[k - 1]);
-                }
-            }
- 
-            min = new int[N + 1][N + 1];
- 
-            int[][] visited = new int[N + 1][N + 1];
-            visited[1][1] = 1;
- 
-            sb.append("#" + (i + 1) + " ");
- 
-            int result = move(visited);
- 
-            sb.append(result).append("\n");
+    private static StringBuilder sb = new StringBuilder();
+    private static int T, N, answer;
+    private static int[][] arr;
+    private static int[][] dp;
+    private static int[] dx = {0, 0, -1, 1};
+    private static int[] dy = {1, -1, 0, 0};
+
+    static class Node {
+        private int x;
+        private int y;
+        private int cost;
+
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
         }
- 
-        //출력
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
     }
- 
-    private static int move(int[][] visited) {
-        int result = Integer.MAX_VALUE;
- 
-        queue.clear();
-        queue.offer(1);
-        queue.offer(1);
- 
-        while (!queue.isEmpty()) {
-            int x = queue.poll();
-            int y = queue.poll();
- 
-            if (x == N && y == N) {
-                result = Math.min(result, min[x][y]);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+            arr = new int[N][N];
+            dp = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(dp[i], 99999999);
             }
- 
-            if (result <= min[x][y]) continue;
- 
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
- 
-                if (nx < 1 || ny < 1 || nx > N || ny > N) continue;
- 
-                // 방문 안 했거나, 방문했더라도 최소 비용 갱신이 가능하다면
-                if (visited[nx][ny] == 0 || min[nx][ny] > min[x][y] + map[nx][ny]) {
- 
-                    //최소값 갱신
-                    min[nx][ny] = min[x][y] + map[nx][ny];
- 
-                    // 방문처리 및 새로운 정점 큐에 넣기
-                    visited[nx][ny] = 1;
-                    queue.offer(nx);
-                    queue.offer(ny);
+
+            for (int n = 0; n < N; n++) {
+                String string = br.readLine();
+
+                for (int m = 0; m < N; m++) {
+                    arr[n][m] = string.charAt(m) - '0';
+                }
+            }
+
+            dfs();
+
+            answer = dp[N - 1][N - 1];
+
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs() {
+        Queue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+        pq.offer(new Node(0, 0, arr[0][0]));
+
+        while (!pq.isEmpty()) {
+            Node polled = pq.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int newY = polled.y + dy[i];
+                int newX = polled.x + dx[i];
+
+                if (newY < 0 || newY >= N || newX < 0 || newX >= N) {
+                    continue;
+                }
+
+                int newCost = polled.cost + arr[newY][newX];
+
+                if (dp[newY][newX] > newCost) {
+                    dp[newY][newX] = newCost;
+                    pq.offer(new Node(newX, newY, newCost));
                 }
             }
         }
- 
-        return result;
     }
 }
