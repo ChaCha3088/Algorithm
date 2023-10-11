3d2194e16c299458f6f0029d4428a6a166a48c2d
Cha Cha <cha3088@gmail.com>
2023-10-11 16:43:26 +0900

    [D2] Title: 나무 높이, Time: 124 ms, Memory: 19,840 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -2,119 +2,98 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Solution {
     private static StringBuilder sb = new StringBuilder();
-    private static int max;
-    private static int[] heights;
+    private static int T, N, max;
+    private static List<Integer> arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
+
         for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+            arr = new ArrayList<>();
             max = 0;
-            int left = 0;
 
             // 입력
-            int N = Integer.parseInt(br.readLine());
-            heights = new int[N];
-
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int n = 0; n < N; n++) {
-                heights[n] = Integer.parseInt(st.nextToken());
-                left += heights[n];
+                int input = Integer.parseInt(st.nextToken());
 
-                // 최대값 찾기
-                if (heights[n] > max) {
-                    max = heights[n];
+                arr.add(input);
+
+                if (max < input) {
+                    max = input;
                 }
             }
 
-            int total = N * max;
-            left = total - left;
-            int day = 0;
-
-            List<Integer> leftOvers = new ArrayList();
-
-            // 남은 수
-            for (int n = 0; n < N; n++) {
-                int leftToGrow = max - heights[n];
-
-                if (leftToGrow >= 1) {
-                    leftOvers.add(leftToGrow);
+            for (int i = arr.size() - 1; i >= 0; i--) {
+                if (arr.get(i) >= max) {
+                    arr.remove(i);
                 }
             }
 
-            // 정렬
-            Collections.sort(leftOvers);
+            Collections.sort(arr, Collections.reverseOrder());
 
-            while (!leftOvers.isEmpty()) {
-                day += 1;
+            int days = 0;
+            while (arr.size() > 0) {
+                days += 1;
 
-                // 짝수 날일 때
-                if (day % 2 == 0) {
-                    // 하나 남았을 때
-                    if (leftOvers.size() == 1) {
-                        // 2 이상 남았으면
-                        if (leftOvers.get(0) >= 2) {
-                            leftOvers.set(0, leftOvers.get(0) - 2);
-
-                            // 다 자랐으면
-                            if (leftOvers.get(0) == 0) {
-                                leftOvers.remove(0);
-                            }
+                // 홀수일 때
+                if (days % 2 == 1) {
+                    // 1개만 남았을 땐
+                    if (arr.size() == 1) {
+                        // 차가 2 아니면
+                        if (max - arr.get(0) != 2) {
+                            // 더해주고
+                            arr.set(0, arr.get(0) + 1);
                         }
-                        // 1 남았으면, 다음 날에 물 준다.
-
                     }
-                    // 두개 이상 남았을 때
-                    else if (leftOvers.size() >= 2) {
-                        for (int i = 0; i < leftOvers.size(); i++) {
-                            if (leftOvers.get(i) >= 2) {
-                                leftOvers.set(i, leftOvers.get(i) - 2);
+                    else {
+                        // 더해주고
+                        arr.set(0, arr.get(0) + 1);
+                    }
+                }
+                // 짝수일 때
+                else {
+                    if (arr.size() == 1) {
+                        int diff = max - arr.get(0);
 
-                                if (leftOvers.get(i) == 0) {
-                                    leftOvers.remove(i);
+                        if (diff >= 2) {
+                            arr.set(0, arr.get(0) + 2);
+                        }
+                    }
+                    else {
+                        for (int i = 0; i < arr.size(); i++) {
+                            int diff = max - arr.get(i);
+
+                            if (diff >= 2) {
+                                arr.set(i, arr.get(i) + 2);
+                                if (arr.get(i) >= max) {
+                                    arr.remove(i);
                                 }
-
                                 break;
                             }
                         }
                     }
                 }
-                // 홀수 날일 때
-                else {
-                    // 하나 남았을 때
-                    if (leftOvers.size() == 1) {
-                        // 2 남은거 아니면 물 주기
-                        if (leftOvers.get(0) != 2) {
-                            leftOvers.set(0, leftOvers.get(0) - 1);
 
-                            if (leftOvers.get(0) == 0) {
-                                leftOvers.remove(0);
-                            }
-                        }
-                    }
-                    // 두개 이상 남았을 때
-                    else if (leftOvers.size() >= 2) {
-                        leftOvers.set(0, leftOvers.get(0) - 1);
-
-                        if (leftOvers.get(0) == 0) {
-                            leftOvers.remove(0);
-                        }
-                    }
-                }
+                removeFinished();
             }
 
-            sb.append("#").append(t).append(" ").append(day).append("\n");
+            sb.append("#").append(t).append(" ").append(days).append("\n");
         }
 
         System.out.println(sb);
+    }
+
+    private static void removeFinished() {
+        if (arr.get(0) >= max) {
+            arr.remove(0);
+        }
     }
 }
