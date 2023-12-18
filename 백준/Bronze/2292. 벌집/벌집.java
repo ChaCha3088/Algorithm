840b6fbff1783a83eb90072a9e823e3aa5f9b012
Cha Cha <cha3088@gmail.com>
2023-12-18 13:47:27 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static StringBuffer sb = new StringBuffer();
+    private static int answer = 1;
+    private static int max = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        while (N > max) {
+            max += (answer) * 6;
+
+            answer += 1;
+        }
+
+        System.out.println(answer);
+    }
+}
