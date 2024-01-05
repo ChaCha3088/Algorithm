1e078091cd6bf59b1235d2da8125793f65e61d0e
Cha Cha <cha3088@gmail.com>
2024-01-05 09:00:55 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 24060 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    private static Deque<Integer> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        queue = new ArrayDeque<>(N);
+
+        for (int i = 1; i <= N; i++) {
+            queue.offerLast(i);
+        }
+
+        while (true) {
+            if (queue.size() <= 1) {
+                break;
+            }
+            queue.pollFirst();
+
+            if (queue.size() <= 1) {
+                break;
+            }
+            queue.offerLast(queue.pollFirst());
+        }
+
+        System.out.println(queue.pollFirst());
+    }
+}
