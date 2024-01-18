8a681022c89df6e5cfb4faaaf83fcb3ddb74c06f
Cha Cha <cha3088@gmail.com>
2024-01-18 20:39:52 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 156 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        int pt = 0;
+
+        int base = 0;
+        while (base++ <= 30000) {
+            String tmp = String.valueOf(base);
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == input.charAt(pt))
+                    pt++;
+
+                if (pt == input.length()) {
+                    System.out.println(base);
+                    return;
+                }
+            }
+        }
+    }
+}
