9c1f3ec81d776fd90538b52eb6493fd16c470fd1
Cha Cha <cha3088@gmail.com>
2024-03-07 22:35:33 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,5 +1,5 @@
 -- 이름에 "EL"이 들어가는 개의 아이디와 이름을 조회
 SELECT animal_id, name from animal_ins
-where animal_type = 'Dog' and name like '%el%' or '%El%' or '%eL' or '%EL%'
+where animal_type = 'Dog' and lower(name) like '%el%' or '%El%' or '%eL' or '%EL%'
 -- 이름 순으로 조회
 order by name
