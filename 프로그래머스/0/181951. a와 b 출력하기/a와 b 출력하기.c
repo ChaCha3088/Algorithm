4e428bd720e20333fba2be0a65b319f08c91c27f
Cha Cha <cha3088@gmail.com>
2024-06-14 15:19:01 +0900

    [level 0] Title: a와 b 출력하기, Time: 1.59 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.c
@@ -0,0 +1,17 @@
+// 정수 a와 b가 주어집니다. 
+// 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+
+#include <stdio.h>
+
+int main(void) {
+    // 정수 a, b 선언
+    int a;
+    int b;
+
+    // 정수 a, b 입력받기
+    scanf("%d %d", &a, &b);
+
+    // 정수 a, b 출력하기
+    printf("a = %d\nb = %d\n", a, b);
+    return 0;
+}
