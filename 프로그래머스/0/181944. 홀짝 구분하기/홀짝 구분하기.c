ff7bcb97c81e62751d1d9843260fd351b41e1884
Cha Cha <cha3088@gmail.com>
2024-06-14 15:37:39 +0900

    [level 0] Title: 홀짝 구분하기, Time: 1.52 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.c
@@ -0,0 +1,16 @@
+// 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+
+    if (a % 2 == 0) {
+        printf("%d is even\n", a);
+    } else {
+        printf("%d is odd\n", a);
+    }
+
+    return 0;
+}
