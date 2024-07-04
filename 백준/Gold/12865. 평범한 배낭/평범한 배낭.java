da900ec08cca39ae6a30b9327e19132fee442c10
Cha Cha <cha3088@gmail.com>
2024-07-04 17:29:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 180 ms, Memory: 53760 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb = new StringBuilder();
-    private static int N, K;
-    private static int dp[][], w[], v[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        dp = new int[N + 1][K + 1];
-
-        w = new int[N + 1];
-        v = new int[N + 1];
+        int[][] arr = new int[N + 1][2]; // 무게, 가치
+        int[][] dp = new int[N + 1][K + 1];
 
         for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= K; j++) {
-                dp[i][j] = dp[i - 1][j];
 
-                // 남는 무게가 존재할 때
-                if (j - w[i] >= 0) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                // i번째 무게를 더 담을 수 없는 경우
+                if(arr[i][0] > j) {
+                    dp[i][j] = dp[i - 1][j];
                 }
+                // i번째 무게를 더 담을 수 있는 경우
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - arr[i][0]] + arr[i][1]);
+                }
+
             }
         }
-
         System.out.println(dp[N][K]);
     }
 }
