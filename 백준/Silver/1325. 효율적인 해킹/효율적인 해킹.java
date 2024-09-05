d295f82262319ceaa17789a5c0a5b539c2b60439
Cha Cha <cha3088@gmail.com>
2024-09-05 16:40:19 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10696 ms, Memory: 317208 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,77 +1,78 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.util.*;
+import java.util.stream.Stream;
 
-
-public class Main {
-    private static StringBuilder sb = new StringBuilder();
-    private static List<List<Integer>> list = new ArrayList();;
-    private static boolean[] visited;
-    private static int max;
-    private static int count;
-    private static int N, M;
+class Main {
+    private static Map<Integer, Integer> map = new LinkedHashMap<>();
+    private static List<Integer>[] arr = new List[10_001];
+    private static boolean[] visited = new boolean[10_001];
+    private static StringBuffer sb = new StringBuffer();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= N; i++) {
-            list.add(new ArrayList());
+        for (int i = 1; i < 10_001; i++) {
+            arr[i] = new ArrayList<>();
         }
 
-        // 입력
-        for (int m = 1; m <= M; m++) {
+        for (int m = 0; m < M; m++) {
             st = new StringTokenizer(br.readLine());
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
 
-            list.get(B).add(A);
-        }
-
-        max = Integer.MIN_VALUE;
-
-        int[] result = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            count = 0;
-            bfs(i);
-            result[i] = count;
-            max = Math.max(count, max);
+            arr[second].add(first);
         }
 
         for (int i = 1; i <= N; i++) {
-            if (result[i] == max) {
-                sb.append(i + " ");
+            visited = new boolean[10_001];
+            visited[i] = true;
+
+            if (!arr[i].isEmpty()) {
+                Deque<Integer> stack = new LinkedList<>();
+                stack.offerLast(i);
+
+                int result = 1;
+
+                while (!stack.isEmpty()) {
+                    int polled = stack.pollLast();
+
+                    for (int e : arr[polled]) {
+                        if (!visited[e]) {
+                            result += 1;
+                            visited[e] = true;
+                            stack.offerLast(e);
+                        }
+                    }
+                }
+
+                map.put(i, result);
             }
         }
+
+        // map의 value로 내림차순 정렬
+        List<Map.Entry<Integer, Integer>> entries = new LinkedList<>(map.entrySet());
+        Collections.sort(entries, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        List<Integer> answers = new LinkedList<>();
+        int max = entries.get(0).getValue();
+
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            if (entry.getValue() == max) {
+                answers.add(entry.getKey());
+            }
+        }
+
+        answers.sort(Integer::compareTo);
+
+        answers.forEach(e -> sb.append(e).append(" "));
 
         System.out.println(sb);
-    }
-
-    private static void bfs(int index) {
-        Queue<Integer> queue = new LinkedList();
-        queue.add(index);
-        visited[index] = true;
-
-        while (!queue.isEmpty()) {
-            int poll = queue.poll();
-
-            for (int i : list.get(poll)) {
-                // 방문 안했으면
-                if (!visited[i]) {
-                    queue.add(i);
-                    visited[i] = true;
-                    count += 1;
-                }
-            }
-        }
     }
 }
